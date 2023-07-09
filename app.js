d89006e12089e1f0b7169cb64ce7caab363de25a
Dhruv Vyas <vyasdhruvh@gmail.com>
2023-07-09 12:06:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,109 +1,217 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var canvas = document.getElementById('renderCanvas');
-    var engine = new BABYLON.Engine(canvas, true);
-    var scene, cube, faceMaterial, pickedFace, faceIndex;
+document.addEventListener('DOMContentLoaded', function() {
+	var canvas = document.getElementById('renderCanvas');
+	var engine = new BABYLON.Engine(canvas, true);
+	var scene;
+	var resetPositions = [];
+	var cubePositions = [];
+	var cube;
+	var currentFaceElement = document.getElementById('currentFace');
   
-    function createScene() {
-      scene = new BABYLON.Scene(engine);
-      var camera = new BABYLON.ArcRotateCamera(
-        'camera',
-        0,
-        0,
-        10,
-        new BABYLON.Vector3(0, 0, 0),
-        scene
-      );
-      camera.attachControl(canvas, true);
+	var createScene = function() {
+	  scene = new BABYLON.Scene(engine);
+	  const camera = new BABYLON.ArcRotateCamera(
+		'Camera',
+		0,
+		0,
+		10,
+		new BABYLON.Vector3(0, 0, 0),
+		scene
+	  );
+	  camera.setPosition(new BABYLON.Vector3(0, 5, 10));
+	  camera.attachControl(canvas, true);
+	  cube = BABYLON.MeshBuilder.CreateBox('box', { size: 2 }, scene);
+	  let faceMaterial = new BABYLON.StandardMaterial('faceMaterial', scene);
+	  faceMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+	  cube.material = faceMaterial;
+	  cube.isPickable = true;
+	  cube.computeWorldMatrix(true);
+	  cube.updateFacetData();
+	  cube.enableEdgesRendering();
+	  cube.edgesWidth = 5;
   
-      cube = BABYLON.MeshBuilder.CreateBox('cube', {}, scene);
-      cube.enableEdgesRendering();
-      cube.edgesWidth = 5;
+	  cubePositions = cube.getVerticesData(BABYLON.VertexBuffer.PositionKind);
+	  resetPositions = Array.from(cubePositions);
   
-      faceMaterial = new BABYLON.StandardMaterial('faceMaterial', scene);
-      faceMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
-      cube.material = faceMaterial;
-      var dragBehavior = new BABYLON.PointerDragBehavior({ dragAxis: BABYLON.Vector3.Zero() });
-      // Selecting a Face
-      scene.onPointerDown = function (event, pickResult) {
-        if (pickResult.hit && pickResult.pickedMesh === cube) {
-          faceIndex = Math.floor(pickResult.faceId / 2);
-          pickedFace = pickResult.getNormal(true);
-          faceMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0);
-          let dragVector = new BABYLON.Vector3(
-            pickedFace.x,
-            pickedFace.y,
-            pickedFace.z
-          );
-          dragBehavior.options.dragAxis = dragVector;
-          //console.log('Selected face index:', faceIndex);
-          //console.log('Picked face normal:', pickedFace);
-        }
-      };
+	  let abstractPlane = new BABYLON.Plane(0, 1, 0, -15);
+	  var plane = null;
+	  let ind = [];
   
-      dragBehavior.onDragStartObservable.add((event) => {
-        // Get the positions of the vertices of the clicked face before the drag starts
-        positions = cube.getVerticesData(BABYLON.VertexBuffer.PositionKind);
-        indices = cube.getIndices();
-        oldFacePositions = [];
-        // Loop through each vertex of the clicked face and add its position to the oldFacePositions array
-        for (let i = 0; i < 6; i++) {
-          const vertexIndex = indices[3 * faceIndex * 2 + i];
-          const vertexPosition = new BABYLON.Vector3(
-            positions[3 * vertexIndex],
-            positions[3 * vertexIndex + 1],
-            positions[3 * vertexIndex + 2]
-          );
-          if (!oldFacePositions.some((pos) => pos.equals(vertexPosition))) {
-            oldFacePositions.push(vertexPosition);
-          }
-        }
-        //console.log('Old face positions:', oldFacePositions);
-        //console.log('Picked face normal:', pickedFace);
-      });
-      // Extruding the Face
-      dragBehavior.onDragObservable.add(function (event) {
-        if (pickedFace) {
-          var dragDistance = event.dragDistance;
-          var extrusionVector = pickedFace.scale(dragDistance);
-          //If the extrusion vector contains any negative element, the cube scales inwards so making sure all axes are positive
-          if(extrusionVector.x < 0){
-            extrusionVector.x = extrusionVector.x * -1;
-          }
-          else if(extrusionVector.y < 0){
-            extrusionVector.y = extrusionVector.y * -1;
-          }
-          else if(extrusionVector.z < 0){
-            extrusionVector.z = extrusionVector.z * -1;
-          }
-          cube.scaling = cube.scaling.add(extrusionVector);
-          cube.position = cube.position.subtract(extrusionVector.scale(0.5));
-        }
-      });
-      cube.addBehavior(dragBehavior);
+	  // makes cube pickable again
+	  scene.onKeyboardObservable.add(function(event) {
+		if (
+		  event.event.type === 'keydown' &&
+		  (event.event.key === 'r' || event.event.key === 'R')
+		) {
+		  scene.getMeshByName('plane').dispose();
+		  currentFaceElement.textContent = 'No face selected right now';
+		  cube.isPickable = true;
+		}
+	  });
   
-      // Reset Button
-      var resetButton = document.getElementById('resetButton');
-      resetButton.addEventListener('click', function () {
-        faceMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
-        cube.scaling = new BABYLON.Vector3(1, 1, 1);
-        pickedFace = null;
-      });
-    }
+	  scene.onPointerObservable.add((pickResult) => {
+		if (pickResult.type === BABYLON.PointerEventTypes.POINTERDOWN){
+			if (
+				pickResult.pickInfo.hit &&
+				pickResult.pickInfo.pickedMesh.name === 'box'
+			) {
+			  if (!scene.getMeshByName('plane')) {
+				// abstractNormal is the actual plane on which we would project points(vertices) of the cube
+				// we should shift the abstract mathematical plane along with the mesh representation of the plane
+				abstractPlane.normal = pickResult.pickInfo
+				  .getNormal()
+				  .normalize();
+				abstractPlane.d = -BABYLON.Vector3.Dot(
+				  abstractPlane.normal,
+				  pickResult.pickInfo.pickedPoint
+				);
   
-    function runRenderLoop() {
-      engine.runRenderLoop(function () {
-        scene.render();
-      });
-    }
+				// Mesh representation of abstract plane, which is supposed to be invisible
+				plane = BABYLON.MeshBuilder.CreatePlane(
+				  'plane',
+				  { sourcePlane: abstractPlane, size: 2 },
+				  scene
+				); //scene is optional and defaults to the current scene
+				plane.position = pickResult.pickInfo.pickedPoint;
+				let mat = new BABYLON.StandardMaterial('planeMat', scene);
+				mat.diffuseColor = new BABYLON.Color3(0, 0, 0);
+				mat.backFaceCulling = false;
+				mat.alpha = 0.0;
+				plane.material = mat;
   
-    function onResize() {
-      window.addEventListener('resize', function () {
-        engine.resize();
-      });
-    }
+				// add dragBehavior to the plane representation
+				var pointerDragBehavior = new BABYLON.PointerDragBehavior({
+				  dragAxis: new BABYLON.Vector3(0, 0, 1),
+				});
+				pointerDragBehavior.attach(plane);
+				cube.isPickable = false;
+				ind = [];
   
-    createScene();
-    runRenderLoop();
-    onResize();
-  });
+				// get a plane that touches the picked point and store all the indices of the vertices that lie on the plane
+				for (let i = 0; i < cubePositions.length; i += 3) {
+				  let v = new BABYLON.Vector3(
+					cubePositions[i],
+					cubePositions[i + 1],
+					cubePositions[i + 2]
+				  );
+				  if (pointOnPlane(v, abstractPlane)) {
+					ind.push(i);
+				  }
+				}
   
+				//when the plane is dragged, update all the vertices after projecting onto the mathematical plane
+				pointerDragBehavior.onDragObservable.add((event) => {
+				  for (let i = 0; i < ind.length; i++) {
+					let poi = new BABYLON.Vector3(
+					  cubePositions[ind[i]],
+					  cubePositions[ind[i] + 1],
+					  cubePositions[ind[i] + 2]
+					);
+					let pp = proj(abstractPlane, poi);
+					cubePositions[ind[i]] = pp.x;
+					cubePositions[ind[i] + 1] = pp.y;
+					cubePositions[ind[i] + 2] = pp.z;
+				  }
+				  cube.setVerticesData(
+					BABYLON.VertexBuffer.PositionKind,
+					cubePositions
+				  );
+				});
+  
+				//when the plane drag is complete, update all the vertices after projecting onto the mathematical plane so that the last
+				//location of the mesh representation is touched
+				pointerDragBehavior.onDragEndObservable.add((event) => {
+				  for (let i = 0; i < ind.length; i++) {
+					let poi = new BABYLON.Vector3(
+					  cubePositions[ind[i]],
+					  cubePositions[ind[i] + 1],
+					  cubePositions[ind[i] + 2]
+					);
+					let pp = proj(abstractPlane, poi);
+					cubePositions[ind[i]] = pp.x;
+					cubePositions[ind[i] + 1] = pp.y;
+					cubePositions[ind[i] + 2] = pp.z;
+				  }
+				  cube.setVerticesData(
+					BABYLON.VertexBuffer.PositionKind,
+					cubePositions
+				  );
+				  cube.disableEdgesRendering();
+				  cube.enableEdgesRendering();
+				});
+  
+				// Synchronize mathematical plane and its mesh representation
+				plane.onAfterRenderObservable.add(() => {
+				  abstractPlane.d = -BABYLON.Vector3.Dot(
+					plane.position,
+					abstractPlane.normal
+				  );
+				});
+			  }
+			  currentFaceElement.textContent =
+				'Current face: ' +
+				pickResult.pickInfo.faceId +
+				". Press 'R' to reset the face";
+			}
+		}
+	  });
+	  return scene;
+	};
+  
+function proj(plane, point) {
+  let ray = new BABYLON.Ray(point, plane.normal, 100);
+  let intersection = ray.intersectsPlane(plane);
+  // Calculates the intersection between the ray and the plane
+
+  if (intersection !== null) {
+    let projectedPoint = ray.origin.add(ray.direction.scale(intersection));
+    // Calculates the projected point by adding the scaled direction vector of the ray to the ray's origin
+    return projectedPoint;
+  }
+
+  // If the intersection is null (no valid intersection), return null or handle it as needed
+  return null;
+}
+
+  
+	// reset plane vertices to their original positions
+	document
+	  .getElementById('resetButton')
+	  .addEventListener('click', function() {
+		cubePositions = [];
+		cubePositions = Array.from(resetPositions);
+		cube.setVerticesData(
+		  BABYLON.VertexBuffer.PositionKind,
+		  cubePositions
+		);
+		scene.getMeshByName('plane').dispose();
+		cube.isPickable = true;
+		cube.disableEdgesRendering();
+		cube.enableEdgesRendering();
+		currentFaceElement.textContent = 'No face selected right now';
+	  });
+	
+	  // Checks if a point lies on a plane or not
+	  function pointOnPlane(point, plane) {
+		let md = BABYLON.Vector3.Dot(plane.normal, point);
+		if (Math.abs(md + plane.d) < 0.00001) {
+		  return true;
+		}
+		return false;
+	  }
+	
+	  function runRenderLoop() {
+		engine.runRenderLoop(function() {
+		  scene.render();
+		});
+	  }
+	
+	  function onResize() {
+		window.addEventListener('resize', function() {
+		  engine.resize();
+		});
+	  }
+	
+	  createScene();
+	  runRenderLoop();
+	  onResize();
+	});
